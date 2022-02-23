bf3157940ea0463e25005be6ba7f9f5128fddca2
Santiago Guillen Paredes <santiago95gp@gmail.com>
2022-02-23 14:37:37 -0500

    add app class to project

--- app/src/main/java/com/santiago/guillen/composedemopokedexapp/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/santiago/guillen/composedemopokedexapp/presentation/home/HomeActivity.kt
@@ -1,8 +1,12 @@
 package com.santiago.guillen.composedemopokedexapp.presentation.home
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
 
 
 class HomeActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
+    }
 }
