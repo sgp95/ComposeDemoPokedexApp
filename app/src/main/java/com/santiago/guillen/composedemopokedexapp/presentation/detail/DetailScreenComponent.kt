65c5399ed3eb9765ae1abd5f548abfda07bd17d8
Kevin Perez <kevin.perez@tektonlabs.com>
2022-02-24 09:14:31 -0500

    Nuevos Componentes: titulo light, chip buttons outlined. Se agregaron las tipografías que usará la app

--- app/src/main/java/com/santiago/guillen/composedemopokedexapp/presentation/detail/DetailScreenComponent.kt
+++ app/src/main/java/com/santiago/guillen/composedemopokedexapp/presentation/detail/DetailScreenComponent.kt
@@ -1,2 +1,60 @@
 package com.santiago.guillen.composedemopokedexapp.presentation.detail
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.santiago.guillen.composedemopokedexapp.ui.theme.Caption1Ligth
+import com.santiago.guillen.composedemopokedexapp.ui.theme.ChipOutlined
+import com.santiago.guillen.composedemopokedexapp.ui.theme.ComposeDemoPokedexAppTheme
+import com.santiago.guillen.composedemopokedexapp.ui.theme.TitleH2Ligth
+
+@Preview(showBackground = true)
+@Composable
+fun ViewPreview() {
+    ComposeDemoPokedexAppTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = Color(0xFFFB6C6C)
+        ) {
+            HeaderPage()
+        }
+    }
+}
+
+@Composable
+fun HeaderPage() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Row(
+            Modifier
+                .width(IntrinsicSize.Max)
+                .padding(start = 12.dp, top = 12.dp)
+        ) {
+            TitleH2Ligth("Charmander")
+        }
+        Row(
+            Modifier
+                .width(IntrinsicSize.Max)
+                .height(IntrinsicSize.Min)
+                .align(Alignment.End)
+                .padding(end = 12.dp)
+        ) {
+            Caption1Ligth("#004")
+        }
+        Row(
+            Modifier
+                .width(IntrinsicSize.Max)
+                .padding(start = 8.dp)
+        ) {
+            ChipOutlined("Posion")
+        }
+    }
+}
+
+
--- app/src/main/java/com/santiago/guillen/composedemopokedexapp/ui/theme/Components.kt
+++ app/src/main/java/com/santiago/guillen/composedemopokedexapp/ui/theme/Components.kt
@@ -1,5 +1,87 @@
 package com.santiago.guillen.composedemopokedexapp.ui.theme
 
-/**
- * This file will contain all the components/widgets shared between the screens.
- */
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+// Texts
+
+@Composable
+fun TitleH2Ligth(
+    text: String,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = text,
+        modifier = modifier,
+        style = TextStyle(
+            fontFamily = fontsLato,
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = 42.sp,
+            color = Color(android.graphics.Color.WHITE)
+        ),
+    )
+}
+
+@Composable
+fun Caption1Ligth(
+    text: String,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = text,
+        modifier = modifier,
+        style = TextStyle(
+            fontFamily = fontsPoppins,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 20.sp,
+            color = Color(android.graphics.Color.WHITE)
+        ),
+    )
+}
+
+// Buttons
+
+@Composable
+fun ChipOutlined(
+    name: String = "Chip",
+    isSelected: Boolean = false,
+    onSelectionChanged: (String) -> Unit = {},
+) {
+    Surface(
+        modifier = Modifier.padding(start = 4.dp),
+        elevation = 0.dp,
+        shape = RoundedCornerShape(16.dp),
+        border= BorderStroke(1.dp, Color.White),
+        color = if (isSelected) Color.Black.copy(alpha = 0.4f) else Color.White.copy(alpha = 0f)
+    ) {
+        Row(modifier = Modifier
+            .toggleable(
+                value = isSelected,
+                onValueChange = {
+                    onSelectionChanged(name)
+                }
+            )
+        ) {
+            Text(
+                text = name,
+                style = MaterialTheme.typography.body2,
+                color = Color.White,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
+
