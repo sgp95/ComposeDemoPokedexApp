7ebc5aa04f940de9a40f4c501b9b451396baa0b3
Kevin Perez <kevin.perez@tektonlabs.com>
2022-02-28 10:15:01 -0500

    LLamando al component de detalle de pokemon en DetailActivity

--- app/src/main/java/com/santiago/guillen/composedemopokedexapp/presentation/detail/DetailActivity.kt
+++ app/src/main/java/com/santiago/guillen/composedemopokedexapp/presentation/detail/DetailActivity.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.pager.ExperimentalPagerApi
 import com.santiago.guillen.composedemopokedexapp.ui.theme.ComposeDemoPokedexAppTheme
 
+@ExperimentalPagerApi
 class DetailActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,29 +21,25 @@ class DetailActivity: ComponentActivity() {
             ComposeDemoPokedexAppTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = Color(0xFFFB6C6C)
                 ) {
-                    Greeting("Kevin")
+                    PokemonDetailLayout()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
+@ExperimentalPagerApi
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeDemoPokedexAppTheme {
         Surface(
             modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colors.background
+            color = Color(0xFFFB6C6C)
         ) {
-            Greeting("Kevin")
+            PokemonDetailLayout()
         }
     }
 }
